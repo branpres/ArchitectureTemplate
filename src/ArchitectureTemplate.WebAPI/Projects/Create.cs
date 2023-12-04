62ae993e61f4b8cb842a57c6cdf601fead44e3fa
branpres <brandon.preston@gmail.com>
2023-12-04 01:14:58 -0500

    Removed unneeded code.

--- src/ArchitectureTemplate.WebAPI/Projects/Create.cs
+++ src/ArchitectureTemplate.WebAPI/Projects/Create.cs
@@ -5,28 +5,29 @@ public class CreateEndpoint : IEndpoint
     public IEndpointRouteBuilder MapEndpoint(IEndpointRouteBuilder builder)
     {
         builder.MapPost("/project", CreateProject)
-        .WithOpenApi(x => new(x)
-        {
-            OperationId = "CreateProject",
-            Description = "Creates a new project. Assigns first project user. Notifies new project user. Creates initial project scope package. Creates project BOM."
-        })
-        .Produces(StatusCodes.Status200OK)
-        .ProducesValidationProblem();
+            .WithOpenApi(x => new(x)
+            {
+                OperationId = "CreateProject",
+                Description = "Creates a new project. Assigns first project user. Notifies new project user. Creates initial project scope package. Creates project BOM."
+            })
+            .Produces(StatusCodes.Status200OK)
+            .ProducesValidationProblem();
 
         return builder;
     }
 
     private async Task<IResult> CreateProject(
         CreateProjectRequest request,
-        [FromServices] IRequestHandler<CreateProjectRequest, CreateProjectResponse> handler,
+        IRequestHandler<CreateProjectRequest, CreateProjectResponse> handler,
         CancellationToken cancellationToken)
     {
         var result = await handler.Handle(request, cancellationToken);
+
         return result.Match(
             response => Results.Created("", response),
             exception => Results.BadRequest(
                 exception.Errors != null
-                    ? new ValidationProblemDetails(exception.Errors)
-                    : new ValidationProblemDetails()));
+                    ? new HttpValidationProblemDetails(exception.Errors)
+                    : new HttpValidationProblemDetails()));
     }
 }
--- src/ArchitectureTemplate.WebAPI/Usings.cs
+++ src/ArchitectureTemplate.WebAPI/Usings.cs
@@ -5,6 +5,5 @@ global using ArchitectureTemplate.Application.UseCases;
 global using ArchitectureTemplate.Application.UseCases.Projects.Create;
 global using ArchitectureTemplate.WebAPI;
 global using FluentValidation;
-global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.Data.Sqlite;
 global using Microsoft.EntityFrameworkCore;
